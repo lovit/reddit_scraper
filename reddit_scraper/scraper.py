1a5779cde317cbdcb493c74c259bb6e313b013a2
lovit <soy.lovit@gmail.com>
2019-01-14 07:36:18 +0900

    Commit yield submission from func

--- reddit_scraper/scraper.py
+++ reddit_scraper/scraper.py
@@ -1,3 +1,8 @@
+import json
+import math
+import requests
+
+
 def _parse_comment(comment):
     return {
         'author_fullname': comment.author_fullname,
@@ -33,3 +38,87 @@ def parse_submission(submission):
         'selftext': submission.selftext,
         'selftext_html': submission.selftext_html
     }
+
+def parse_idx(strf):
+    return strf[3:]
+
+def get_after_submission_ids(r, after_id, dist):
+    headers = {
+        'accept': '*/*',
+        'accept-encoding': 'gzip, deflate, br',
+        'accept-language': 'ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7',
+        'content-type': 'application/x-www-form-urlencoded',
+        'origin': 'https://www.reddit.com',
+        'referer': 'https://www.reddit.com/r/{}/new/'.format(r),
+        'user-agent': 'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36'
+    }
+    request_base = 'https://gateway.reddit.com/desktopapi/v1/subreddits/{}?rtj=debug&redditWebClient=web2x&app=web2x-client-production&after=t3_{}&dist={}&layout=card&sort=new&allow_over18=&include='
+    url = request_base.format(r, after_id, dist)
+    r = requests.get(url=url, headers=headers)
+    submission_ids = [parse_idx(idx) for idx in json.loads(r.text)['postIds']]
+    return submission_ids
+
+def yield_submission_from(begin_date, r, dist, max_num=100, sleep=1):
+    """
+    Arguments
+    ---------
+    begin_date : str
+        Begin date str format. 2018-01-01
+    r : str
+        Subreddit name. For example, ['MachineLearning', 'Politics']
+    d : str or int
+        dist idx corresponding r
+    max_num : int
+        Maximum number of submissions (posts) to be scraped
+    sleep : float
+        Sleep time for each submission
+
+    It yields
+    -----
+    submission as json format
+    """
+
+    d_begin = strf_to_datetime(begin_date)
+    # get latest submission idx
+    soup = get_soup('https://www.reddit.com/r/{}/new/'.format(r))
+    after_id = soup.select('div[class^=scrollerItem]')[0].attrs['id']
+    after_id = parse_idx(after_id)
+
+    # set iteration parameters
+    n_tries = math.ceil(max_num/25)
+    n_posts = 0
+    outdate = False
+
+    # iterate
+    for _ in range(n_tries):
+        # check max_num
+        if n_posts >= max_num:
+            break
+
+        # check begin date
+        if outdate:
+            print('Stop scrapping. {} / {} posts was scrapped'.format(n_posts, max_num))
+            print('The oldest submission has been created after {}'.format(begin_date))
+            break
+
+        # get submission ids
+        submission_ids = get_after_submission_ids(r, after_id, dist)
+
+        for idx in submission_ids:
+            if n_posts >= max_num:
+                break
+            submission = reddit.submission(id=idx)
+
+            # check begin date
+            created = unixtime_to_datetime(submission.created_utc)
+            if d_begin > created:
+                outdate = True
+                break
+
+            # parse submission and yield the result
+            json_obj = parse_submission(submission)
+            yield json_obj
+
+            # flush
+            n_posts += 1
+            time.sleep(sleep)
